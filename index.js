45531bac3df8b48944756157427a4577b11512d7
Dhunyellison <dh.unypaulo@gmail.com>
2026-02-28 21:27:07 -0300

    Código limpo para o Render

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ console.log('Iniciando cliente do WhatsApp...');
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        executablePath: isProducao ? '/usr/bin/google-chrome-stable' : undefined,
+        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
     }
 });
